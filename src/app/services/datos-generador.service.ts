dbb1b0e699852b6a5b11fb90684047fdff3e2788
stevengm45 <stevengm45@gmail.com>
2024-01-16 22:54:59 -0500

    se agrega nuevo sidbar y se ajustan estilos

--- src/app/services/datos-generador.service.ts
+++ src/app/services/datos-generador.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatosGeneradorService {
 
+  private API_SERVER = '/certificado'
+
   constructor(private httpClient: HttpClient) { }
+
+  public saveGenerador(generador:any){
+    return this.httpClient.post(environment.API_SERVER+this.API_SERVER, generador);
+  }
 }
