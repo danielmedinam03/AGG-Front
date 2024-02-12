f34f9f1486af209f94556afdbd8ff2d543408d4c
stevengm45 <stevengm45@gmail.com>
2024-02-12 15:21:49 -0500

    se cambia url del back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  API_SERVER:"https://agg-backend-a9bf5457cba2.herokuapp.com"
+  //url del correo suministramos agg
+  //API_SERVER:"https://agg-backend-a9bf5457cba2.herokuapp.com"
+  API_SERVER:"https://aggback-06bfd4b6a04d.herokuapp.com/"
 
 };
