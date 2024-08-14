c64cb5d8a4febf6cb680a91cc9e59ea0014ea818
danielmedinam03 <73619222+danielmedinam03@users.noreply.github.com>
2024-08-14 16:39:41 -0500

    Ajuste url back produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //url del correo suministramos agg
   //API_SERVER:"https://agg-backend-a9bf5457cba2.herokuapp.com"
-  API_SERVER:"https://back-audit-bc7bf862f72c.herokuapp.com/"
+  API_SERVER:"https://back-audit-bc7bf862f72c.herokuapp.com"
 };
