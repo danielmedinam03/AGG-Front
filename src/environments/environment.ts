acf1b531ceb42162265239ed177e87066f51737c
danielmedinam03 <73619222+danielmedinam03@users.noreply.github.com>
2024-10-24 16:33:08 -0500

    Prueba de deploy en pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 export const environment = {
   production: false,
   API_SERVER:"http://localhost:8080"
-  //API_SERVER:"https://agg-backend-a9bf5457cba2.herokuapp.com"
+  //API_SERVER:"https://dev-auditoria-2-646a91187028.herokuapp.com"
 
 
 
